bde1ec25458f7ab570033a6c3d6781b9693b4113
xhdjh <djh@100fen.cn>
2019-01-20 18:00:23 +0800

    learn use fun

--- app/src/main/java/com/golike/topactivity/MainActivity.kt
+++ app/src/main/java/com/golike/topactivity/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.golike.topactivity
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val result = test(2, 10);
+        this.tv_name.text = result.toString() +"学习hello World!!!";
+    }
+
+    fun test(a: Int, b: Int): Int {
+        return a + b
     }
 }
