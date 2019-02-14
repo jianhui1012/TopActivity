f4fd9774630d0c8ca15d38ca5a76b549951283f7
xhdjh <djh@100fen.cn>
2019-02-14 17:37:05 +0800

    增加lottie动画库

--- app/src/main/java/com/golike/topactivity/MainActivity.kt
+++ app/src/main/java/com/golike/topactivity/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.golike.topactivity
 
-import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
-import android.provider.Settings
 import android.support.v7.app.AppCompatActivity
+import com.airbnb.lottie.LottieProperty
+import com.airbnb.lottie.model.KeyPath
+import com.airbnb.lottie.value.LottieValueCallback
 import kotlinx.android.synthetic.main.activity_main.*
 import me.ele.uetool.UETool
 
@@ -14,15 +16,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         UETool.showUETMenu()
-
+        animation_view.addValueCallback(
+                KeyPath("Shape Layer", "Rectangle", "Fill"),
+                LottieProperty.COLOR,
+                LottieValueCallback(Color.RED))
         this.tv_start.setOnClickListener {
             run {
                 // 判断辅助功能是否开启
                 if (!AccessibilityUtil.isAccessibilitySettingsOn(this)) {
                     // 引导至辅助功能设置页面
-                    startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
+                    //startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
                 }
+                animation_view.setAnimation("lottie_data_edit.json")
+                animation_view.playAnimation()
+
             }
         }
+
+
     }
 }
